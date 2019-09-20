60b31bf7215f87070aad76cecf71f49897edcecc
72crm <493466659@qq.com>
2019-09-20 16:04:35 +0800

    20190920

---
+++ application/admin/model/Center.php
@@ -0,0 +1,18 @@
+<?php
+// +----------------------------------------------------------------------
+// | Description: 
+// +----------------------------------------------------------------------
+// | Author:  Michael_xu | gengxiaoxu@5kcrm.com
+// +----------------------------------------------------------------------
+
+namespace app\admin\model;
+
+use app\admin\model\Common;
+
+class Center extends Common 
+{
+	public function userData($param)
+	{
+		return true;
+	}	
+}
